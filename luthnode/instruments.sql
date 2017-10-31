e2f473026941e6f5d39b7f105d1468b0072a33d6
Florie Lesueur <florie.lesueur@gmail.com>
2017-10-31 15:58:18 +0100

    fake

--- luthnode/instruments.sql
+++ luthnode/instruments.sql
@@ -11,6 +11,6 @@ CREATE TABLE modeles
     marque VARCHAR(45) NOT NULL,
     bois_utilise VARCHAR(45) NOT NULL,
     description VARCHAR(500) NOT NULL
-)
+);
 
 INSERT INTO posts VALUES (NULL, 'mon premier instrument', 'xxx', 'marque', 'bois utilisé', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod');
