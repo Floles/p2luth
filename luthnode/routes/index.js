d09c8399cc8c56fff94c350eadcd6616aba96025
codingk8 <codingk8@gmail.com>
2017-11-06 17:19:43 +0100

    Dans le fichier index.js, ajout de la route vers la pages Mes produits en front

--- luthnode/routes/index.js
+++ luthnode/routes/index.js
@@ -7,40 +7,44 @@ const connection = mysql.createConnection(config);
 
 connection.connect();
 
-/* A valider GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+/* GET homepage */
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
-/* A valider GET page Mes créations. */
-router.get('/mescreations', function(req, res, next) {
-  res.render('gabarit1');
+/* GET page Mes créations */
+router.get('/mescreations', function (req, res, next) {
+    res.render('gabarit1');
 });
 
-/* A valider GET page Me contacter. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact');
+/* GET page Mes produits */
+router.get('/mesproduits', function (req, res, next) {
+    res.render('gabaritpdt');
 });
 
-/* A valider GET page Produit. */
-router.get('/produit/product-:id([\\d+])', function(req, res, next) {
-  connection.query('SELECT * FROM products WHERE id_products = ?;', [req.params.id], function(error, results, fields){
-		if (error) {
-			console.log(error);
-		};
-		if (results.length == 0){
-			res.sendStatus(404);
-		}
-		res.render('gabaritpdt', {
-        	products:results[0]
-      	});
-	});
-	
+/* GET page Produit, détails d'un produit */
+router.get('/produit/product-:id([\\d+])', function (req, res, next) {
+    connection.query('SELECT * FROM products WHERE id_products = ?;', [req.params.id], function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        };
+        if (results.length == 0) {
+            res.sendStatus(404);
+        }
+        res.render('gabaritpdt', {
+            products: results[0]
+        });
+    });
 });
 
-router.get('/mentions-legales', function(req, res, next) {
-  res.render('mentionslegales');
+/* GET page Me contacter */
+router.get('/contact', function (req, res, next) {
+    res.render('contact');
 });
 
+/* GET page Mentions légales */
+router.get('/mentions-legales', function (req, res, next) {
+    res.render('mentionslegales');
+});
 
 module.exports = router;
