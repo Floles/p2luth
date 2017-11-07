de53916afe5e0939f34726cdc4e65664e9db296a
codingk8 <codingk8@gmail.com>
2017-11-07 18:13:57 +0100

    Admin des textes, routes et pages OK Reste à rendre les pages concernées dynamiques

--- luthnode/routes/admin.js
+++ luthnode/routes/admin.js
@@ -1,152 +1,193 @@
-
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 const config = require('../config.js');
 const fs = require('fs');
-const multer  = require('multer');
-const upload = multer({ dest: 'tmp/' });
+const multer = require('multer');
+const upload = multer({
+    dest: 'tmp/'
+});
 const connection = mysql.createConnection(config);
 
 connection.connect();
 
 
-// GET /admin 
-router.get('/', function(req, res, next) {
-	// Liste des produits
-	connection.query('SELECT * FROM products;', function(error, results, fields){
-		if (error) {
-			console.log(error);
-		}else {
-			console.log(results)
-			res.render('admin-index', {products:results});
-		};
-	});
-	
+// GET /admin pour les produits
+router.get('/', function (req, res, next) {
+    // Liste des produits
+    connection.query('SELECT * FROM products;', function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(results)
+            res.render('admin-index', {
+                products: results
+            });
+        };
+    });
+});
+
+// GET /admin pour les textes
+router.get('/details', function (req, res, next) {
+    // Liste des textes
+    connection.query('SELECT * FROM commercial_details;', function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(results)
+            res.render('admin-details', {
+                commercial_details: results
+            });
+        };
+    });
 });
 
 // GET /admin/create
-router.get('/create', function(req, res, next) {
-	// WYSIWYG
-	res.render('admin_create');
+router.get('/create', function (req, res, next) {
+    // WYSIWYG
+    res.render('admin_create');
 });
 
 // POST /admin/create
-router.post('/create', upload.single('image'), function(req, res, next) {
-	// Création d'article
-	if (req.file.size < (4*1024*1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpeg')) {
-		fs.rename(req.file.path,'public/images/'+ req.file.originalname);
-	} else {
-		res.send('Vous avez fait une erreur dans le téléchargement');
-	}
-	connection.query('INSERT INTO products VALUES(NULL, ?, ?, ?, ?, ?, ?, NULL);',
-	[req.body.product, req.body.reference, req.body.marque, req.body.bois_utilise, req.body.description, req.file.originalname],
-	function(error, results, fields){
-		if (error) {
-			console.log(error);
-		} else {
-			res.redirect('/admin');
-		};
-	});
+router.post('/create', upload.single('image'), function (req, res, next) {
+    // Création d'article
+    if (req.file.size < (4 * 1024 * 1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpeg')) {
+        fs.rename(req.file.path, 'public/images/' + req.file.originalname);
+    } else {
+        res.send('Vous avez fait une erreur dans le téléchargement');
+    }
+    connection.query('INSERT INTO products VALUES(NULL, ?, ?, ?, ?, ?, ?, NULL);', [req.body.product, req.body.reference, req.body.marque, req.body.bois_utilise, req.body.description, req.file.originalname],
+        function (error, results, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.redirect('/admin');
+            };
+        });
 });
 
 // GET update
-router.get('/update/produit:id_products(\\d+)',function(req, res){
-	connection.query('SELECT * FROM products WHERE id_products = ?;', [req.params.id_products], function(error, results){
+router.get('/update/produit:id_products(\\d+)', function (req, res) {
+    connection.query('SELECT * FROM products WHERE id_products = ?;', [req.params.id_products], function (error, results) {
 
-		res.render('admin-update', {
-			products: results[0]
-		});
-	});
+        res.render('admin-update', {
+            products: results[0]
+        });
+    });
 });
 
 // image à remettre dans la connection query (req.body.image, image = ?)
-router.post('/update/produit:id_products(\\d+)', upload.single('image'), function(req, res){
-	if (req.file.size < (4*1024*1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpeg')) {
-		fs.rename(req.file.path,'public/images/'+ req.file.originalname);
-	} else {
-		res.send('Vous avez fait une erreur dans le téléchargement');
-	}
-	connection.query('UPDATE products SET product = ?, reference = ?, marque = ?, bois_utilise = ?, description = ?, image = ? WHERE id_products = ?;', 
-		[req.body.product, req.body.reference, req.body.marque, req.body.bois_utilise, req.body.description, req.file.originalname, req.params.id_products], function(error){
-		if (error) {
-			console.log(error);
-		} else {
-			res.redirect('/admin');
-		}
-	});
+router.post('/update/produit:id_products(\\d+)', upload.single('image'), function (req, res) {
+    if (req.file.size < (4 * 1024 * 1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpeg')) {
+        fs.rename(req.file.path, 'public/images/' + req.file.originalname);
+    } else {
+        res.send('Vous avez fait une erreur dans le téléchargement');
+    }
+    connection.query('UPDATE products SET product = ?, reference = ?, marque = ?, bois_utilise = ?, description = ?, image = ? WHERE id_products = ?;', [req.body.product, req.body.reference, req.body.marque, req.body.bois_utilise, req.body.description, req.file.originalname, req.params.id_products], function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/admin');
+        }
+    });
+});
+
+// GET update des textes du site
+router.get('/textes/detail:id_cd(\\d+)', function (req, res) {
+    connection.query('SELECT * FROM commercial_details WHERE id_cd = ?;', [req.params.id_cd], function (error, results) {
+
+        res.render('admin-textes', {
+            detail: results[0]
+        });
+    });
+});
+
+// image à remettre dans la connection query (req.body.image, image = ?)
+router.post('/textes/detail:id_cd(\\d+)', upload.single('image'), function (req, res) {
+    if (req.file.size < (4 * 1024 * 1024) && (req.file.mimetype == 'image/png' || req.file.mimetype == 'image/jpeg')) {
+        fs.rename(req.file.path, 'public/images/' + req.file.originalname);
+    } else {
+        res.send('Vous avez fait une erreur dans le téléchargement');
+    }
+    connection.query('UPDATE commercial_details SET name = ?, content = ?, image = ? WHERE id_cd = ?;', [req.body.name, req.body.content, req.file.originalname, req.params.id_cd], function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/admin-details');
+        }
+    });
 });
 
 // Delete
-router.get('/supprimer/produit-:id_products(\\d+)',function(req, res){
-	connection.query('DELETE FROM products WHERE id_products = ?;', [req.params.id_products], function(error){
-		if (error) {
-			console.log(error);
-		} else {
-			res.redirect('/admin');
-		}
+router.get('/supprimer/produit-:id_products(\\d+)', function (req, res) {
+    connection.query('DELETE FROM products WHERE id_products = ?;', [req.params.id_products], function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/admin');
+        }
 
-	});
+    });
 });
 
 
 // page de login
 
-router.get('/', function(req, res, next) {
-	// Page de connexion
+router.get('/', function (req, res, next) {
+    // Page de connexion
 
-	connection.query(`select * from users where name= ?;`,[req.body.toto], function (error, results, fields) {
-	 	 res.render('index', { 
-	 	 	title: 'Express',
-	 	 	error : JSON.stringify(error),
-	 	 	results: JSON.stringify(results), 
-	 	 	fields : JSON.stringify(fields)
-	 	 	 });
- 	 
-	});
+    connection.query(`select * from users where name= ?;`, [req.body.toto], function (error, results, fields) {
+        res.render('index', {
+            title: 'Express',
+            error: JSON.stringify(error),
+            results: JSON.stringify(results),
+            fields: JSON.stringify(fields)
+        });
 
-  
+    });
+
+
 });
 
-router.post('/', function(req, res, next) {
-	// Ici on gère les informations de l'utilisateur
+router.post('/', function (req, res, next) {
+    // Ici on gère les informations de l'utilisateur
 
-	//res.send(req.body.username);
-	//res.send(req.body['username']);
+    //res.send(req.body.username);
+    //res.send(req.body['username']);
 
-	// Tester si l'utilisateur existe en BDD  -> Comparer le nom (login) / le password
-	let login= req.body.username;
-	let password = req.body.password ;
-	// select name, password from users where name='${var}' and password='wild';
-	//` text ${var} fzoeijfzeoj ${var2}` 
+    // Tester si l'utilisateur existe en BDD  -> Comparer le nom (login) / le password
+    let login = req.body.username;
+    let password = req.body.password;
+    // select name, password from users where name='${var}' and password='wild';
+    //` text ${var} fzoeijfzeoj ${var2}` 
 
-	connection.query(`select * from users where name= "${login}" 
+    connection.query(`select * from users where name= "${login}" 
 		and password="${password}";`, function (error, results, fields) {
- 	 if (results.length==0) {
- 	 	res.send("Erreur");
- 	 }else{
- 	 	req.session.connect=true;
- 	 	res.redirect("/admin");
+        if (results.length == 0) {
+            res.send("Erreur");
+        } else {
+            req.session.connect = true;
+            res.redirect("/admin");
 
- 	 }
- 	 
-	});
-	// Si faux on lui envoie un message pour l'informer 
-	// Si vrai -> On ouvre la session & on le redirige sur /admin 
+        }
 
-  	 
+    });
+    // Si faux on lui envoie un message pour l'informer 
+    // Si vrai -> On ouvre la session & on le redirige sur /admin 
+
+
 });
 
-router.get('/adminToto', function(req, res, next) {
-	// Hello session !
-	// res.send(req.session.connect);
-	// Si la personne est connectée on affiche la page 
-	// Si la personne n'est pas connectée on le redirige sur la page de connexion
-  if(req.session.connect) {
-  	res.render('admin');
-  } else {
-  	res.redirect("/");
-  }
+router.get('/adminToto', function (req, res, next) {
+    // Hello session !
+    // res.send(req.session.connect);
+    // Si la personne est connectée on affiche la page 
+    // Si la personne n'est pas connectée on le redirige sur la page de connexion
+    if (req.session.connect) {
+        res.render('admin');
+    } else {
+        res.redirect("/");
+    }
 });
 
 /* A compléter GET page admin error.
