ee5c3ae0cae8ac27be498869e2ed1a449ac784b3
Florie Lesueur <florie.lesueur@gmail.com>
2017-11-07 18:37:07 +0100

    envoi email ok

--- luthnode/routes/index.js
+++ luthnode/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 const config = require('../config.js');
+const nodemailer = require('nodemailer');
 
 const connection = mysql.createConnection(config);
 
@@ -41,6 +42,35 @@ router.get('/produit/product-:id([\\d+])', function (req, res, next) {
 router.get('/contact', function (req, res, next) {
     res.render('contact');
 });
+
+router.post('/contact', function (req, res, next) {
+    var transport = nodemailer.createTransport({
+        host : "smtp.mailtrap.io",
+        port : 2525,
+        auth : {
+          user: "dfd01726c10a1e",
+          pass: "468515e201bd79"
+        }
+    });
+    var mailOptions = {
+        from: req.body.email, // Expediteur
+        to: "luthier@yopmail.com", // Destinataires
+        subject: "Demande d'informations", // Sujet
+        text: req.body.message, // plaintext body
+        html: "<p>" + req.body.message + "</p>" // html body
+    };
+    console.log(req.body);
+    transport.sendMail(mailOptions, function (error, response){
+        if(error){
+            return console.log(error);
+        }
+        console.log("Message sent : " + response.message);
+      });
+    transport.close();
+    res.redirect('/contact');
+});
+
+
 
 /* GET page Mentions légales */
 router.get('/mentions-legales', function (req, res, next) {
